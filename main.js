257684293d0cebef5da5f98eafca546960ccceec
calmighted <manishh@tutanota.com>
2022-08-29 11:15:35 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// const fibonacci = (n) => {
+// 	let arr = [0, 1];
+// 	let newNum;
+// 	for (let i = 0; i < n - 2; i++) {
+// 		newNum = arr[i] + arr[i + 1];
+// 		arr.push(newNum);
+// 	}
+// 	return arr;
+// };
+
+// console.log(fibonacci(7));
+
+
+// const factorial = (n) => {
+//     let mul = 1
+//     for (let i = 1; i <= n; i++) {
+//         mul = mul * i
+        
+//     }
+//     return mul
+// }
+
+
+// console.log(factorial(1))
+
+
+const isPrime = (n) => {
+    
+}
