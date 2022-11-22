cc0abeae27dc4c3b79e0f3c79ca69e3077b0d02e
calmighted <manishh@tutanota.com>
2022-11-22 11:52:29 +0530

    added overview and sorting algos

--- search-Algorithms/binarySearch.js
+++ search-Algorithms/binarySearch.js
@@ -1,12 +1,27 @@
 // only works on sorted array
 
 
+let mid
+
 const binarySearch = (arr, t ) => {
-    
+    mid = arr.length / 2 === 0 ? arr.length / 2 : Math.floor(arr.length / 2)
+    // console.log(arr)
+    // console.log("mid",mid)
+    if(arr[mid] === t){
+        return mid
+    }
+    if(arr[mid] > t){
+        return binarySearch(arr.slice(0,mid),t)
+    }
+    if(arr[mid] < t){
+        return binarySearch(arr.slice(mid),t)
+    }
+
+    return -1
 
     
 }
 
 
 
-console.log(binarySearch([-5,2,6,10,4], 10))
+console.log(binarySearch([-5,2,6,10,4], -512))
